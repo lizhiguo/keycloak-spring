c8d41c81be36242a07d3e36abfde94e6fe3fe4be
victorhugolgr <victorhugolgr@gmail.com>
2017-03-02 11:55:26 -0300

    inicial

---
+++ keycloak-spring/src/main/java/br/com/keycloak/spring/KeycloakSpringApplication.java
@@ -0,0 +1,12 @@
+package br.com.keycloak.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSpringApplication.class, args);
+	}
+}
